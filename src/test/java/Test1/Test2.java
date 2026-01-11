1bf97b2dda70241570a1c5cf3da81b94d60158f5
91820 <91820@DESKTOP-3C141LT>
2026-01-11 19:48:05 +0530

    second commit

---
+++ src/test/java/Test1/Test2.java
@@ -0,0 +1,21 @@
+package Test1;
+
+public class Test2 {
+
+	public static void main(String[] args)
+	{
+		
+		int orgNum = 112233;
+		String org = Integer.toString(orgNum);  //convert number to string
+
+		String rev = "";
+		for (int i = org.length() - 1; i >= 0; i--) 
+		{
+			rev = rev + org.charAt(i);
+		}
+		
+		int revNum = Integer.parseInt(rev);   // convert string to number
+		System.out.println(revNum);
+
+}
+}
